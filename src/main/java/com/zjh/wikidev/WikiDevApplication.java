915f14a25f4add1111ce3613e2036752e17a3eba
wiki <1012542381@qq.com>
2022-05-17 22:20:13 +0800

    1.1——第一次提交项目

---
+++ src/main/java/com/zjh/wikidev/WikiDevApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.wikidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjh/wikidev/WikiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.wikidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
